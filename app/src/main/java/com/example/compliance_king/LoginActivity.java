4b8f56a89bc5a1e0f43468bdc5d3c51b290cd204
knu-ldh <dahee@kunsan.ac.kr>
2020-05-19 00:29:02 +0900

    Add login screen

---
+++ app/src/main/java/com/example/compliance_king/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.example.compliance_king;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        setTitle("LOGIN");
+
+        //ActionBar actionBar = getSupportActionBar();
+        //actionBar.hide();
+    }
+}
