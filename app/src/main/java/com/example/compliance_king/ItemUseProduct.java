9e0784c4978d8c11e80634cbf623110635a70ad4
knu-ldh <dahee@kunsan.ac.kr>
2020-06-06 23:45:09 +0900

    create ListView

---
+++ app/src/main/java/com/example/compliance_king/ItemUseProduct.java
@@ -0,0 +1,13 @@
+package com.example.compliance_king;
+
+public class ItemUseProduct {
+    String name;
+    String date1;
+    String date2;
+
+    public ItemUseProduct(String name, String date1, String date2) {
+        this.name = name;
+        this.date1 = date1;
+        this.date2 = date2;
+    }
+}
